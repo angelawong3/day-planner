902bd36145abd42ef12462890a5c7882c8543d1a
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-05-15 22:26:15 +0100

    pesudo code for js

--- 02-Homework/assets/js/script.js
+++ 02-Homework/assets/js/script.js
@@ -2,4 +2,8 @@
 var today = moment().format("MMMM Do YYYY");
 $("#currentDay").text(today);
 
-// display hour in planner
+// each timeblock is color coded to indicate past, present, or future
+
+// save text inputted in local storage
+
+//saved events persist after refreshing the page
